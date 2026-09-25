19350773abb2464400eaba400cf4a343f23d74ad
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-25 23:53:06 +0000

    IMGLYPluginAutoCaptions version 1.82.2-rc.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "IMGLYPluginAutoCaptions", targets: ["IMGLYPluginAutoCaptions"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.83.0-rc.2"),
+    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.82.2-rc.0"),
   ],
   targets: [
     .target(
